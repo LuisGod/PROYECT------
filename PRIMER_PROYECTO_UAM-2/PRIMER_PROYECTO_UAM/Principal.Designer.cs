9eae187220cdea73649f7f09827f3b23b29ae495
mikeartavia <mikeartavia03@gmail.com>
2015-11-11 21:44:03 -0600

    Mike files c# and database

---
+++ PRIMER_PROYECTO_UAM-2/PRIMER_PROYECTO_UAM/Principal.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PRIMER_PROYECTO_UAM
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bTNCONSULTAR = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnrgscondu = new System.Windows.Forms.Button();
+            this.buttonELIMCOND = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnregistrar
+            // 
+            this.btnregistrar.BackColor = System.Drawing.Color.Coral;
+            this.btnregistrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnregistrar.Location = new System.Drawing.Point(308, 130);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(156, 40);
+            this.btnregistrar.TabIndex = 0;
+            this.btnregistrar.Text = "REGISTRAR CLIENTES";
+            this.btnregistrar.UseVisualStyleBackColor = false;
+            this.btnregistrar.Click += new System.EventHandler(this.btnregistrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Coral;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(568, 87);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BIENVENIDO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 6;
+            // 
+            // bTNCONSULTAR
+            // 
+            this.bTNCONSULTAR.BackColor = System.Drawing.Color.Coral;
+            this.bTNCONSULTAR.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bTNCONSULTAR.Location = new System.Drawing.Point(309, 176);
+            this.bTNCONSULTAR.Name = "bTNCONSULTAR";
+            this.bTNCONSULTAR.Size = new System.Drawing.Size(156, 40);
+            this.bTNCONSULTAR.TabIndex = 4;
+            this.bTNCONSULTAR.Text = "CONSULTAR CLIENTE";
+            this.bTNCONSULTAR.UseVisualStyleBackColor = false;
+            this.bTNCONSULTAR.Click += new System.EventHandler(this.bTNCONSULTAR_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Coral;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(309, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 57);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "VISUALIZAR Y ELIMINAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnrgscondu
+            // 
+            this.btnrgscondu.BackColor = System.Drawing.Color.Coral;
+            this.btnrgscondu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnrgscondu.Location = new System.Drawing.Point(132, 130);
+            this.btnrgscondu.Name = "btnrgscondu";
+            this.btnrgscondu.Size = new System.Drawing.Size(156, 40);
+            this.btnrgscondu.TabIndex = 7;
+            this.btnrgscondu.Text = "REGISTRAR CONDUCTORES";
+            this.btnrgscondu.UseVisualStyleBackColor = false;
+            this.btnrgscondu.Click += new System.EventHandler(this.btnrgscondu_Click);
+            // 
+            // buttonELIMCOND
+            // 
+            this.buttonELIMCOND.BackColor = System.Drawing.Color.Coral;
+            this.buttonELIMCOND.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonELIMCOND.Location = new System.Drawing.Point(132, 176);
+            this.buttonELIMCOND.Name = "buttonELIMCOND";
+            this.buttonELIMCOND.Size = new System.Drawing.Size(156, 40);
+            this.buttonELIMCOND.TabIndex = 8;
+            this.buttonELIMCOND.Text = "ELIMINAR CONDUCTORES";
+            this.buttonELIMCOND.UseVisualStyleBackColor = false;
+            this.buttonELIMCOND.Click += new System.EventHandler(this.buttonELIMCOND_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Coral;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(85, 89);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(133, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 57);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "AGREGAR MONTO RECAUDADO A CONDUCTORES";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(564, 383);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonELIMCOND);
+            this.Controls.Add(this.btnrgscondu);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bTNCONSULTAR);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnregistrar);
+            this.Controls.Add(this.label2);
+            this.Name = "Principal";
+            this.Text = "BIENVENIDO";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnregistrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bTNCONSULTAR;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnrgscondu;
+        private System.Windows.Forms.Button buttonELIMCOND;
+        private System.Windows.Forms.Button button2;
+    }
+}
